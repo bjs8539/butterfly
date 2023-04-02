23abe4a7eb61f9288793590f7a091a46751b24df
Byeon Unseop <ghlejr316@naver.com>
2023-04-02 16:16:18 +0900

    pc 중간 커밋

--- jeongwoo-tax/js/main.js
+++ jeongwoo-tax/js/main.js
@@ -1,97 +1,17 @@
-;(function(){
-    'use strict';
-    var $window = $(window);
-    var $wrap = $('.sec_project_wrap');
+$(document).ready(function() {
 
-    var mainIndex = {
+    const $WINDOW = $(window);
 
+    const MAININDEX = {
         
-        anchorEvt: function() {
-            function pixelToVw(pc, mo) {
-                var result = 1;
-                var w = $window.outerWidth();
-                var isMobile = w < 769;
-                if (pc || mo) {
-                    if (isMobile) {
-                        result = mo >= 0 ? Math.min(mo, mo/720 * w) : Math.max(mo, mo/720 * w);
-                    } else {
-                        result = pc >= 0 ? Math.min(pc, pc/1440 * w) : Math.max(pc, pc/1440 * w);
-                    }
-                }
-
-                return result;
-            }
-
-            // 페이지 내 앵커드버튼 처리
-            $wrap.off('click.anchor').on('click.anchor', '[data-role="btnAnchor"]', function(e) {
-                e.preventDefault();
-                var $this = $(this);
-                var target = $this.attr('data-target');
-                var speed = $this.attr('data-speed') == 0 ? 0 : $this.attr('data-speed') || 500;
-                var scroll = [1,1];
-                var dataScroll = $this.attr('data-scroll');
-                
-                console.log('눌렀따');
-
-                if (dataScroll) {
-                    dataScroll = dataScroll.split('|');
-                    scroll[0] = dataScroll[0];
-                    scroll[1] = dataScroll[1] ? dataScroll[1] : scroll[1];
-                }
-
-                $('html, body').stop().animate({ 
-                    scrollTop:$(target).offset().top + pixelToVw(scroll[0], scroll[1])
-                }, speed);
-            });
-
-            function getParameterByName(name) {
-                name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-                var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-                        results = regex.exec(location.search);
-                return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-            }
-
-            // 페이지 접속시 앵커드 처리 - 필요하면 변경해서 사용
-            var paramsData = {
-                about: {el:$('#sec_about')},
-                rooms: {el:$('#sec_room_pre')},
-                test02: {el:$('.sec_accordian'), scroll:[0, -50]}
-            }
-
-            var anc = getParameterByName('anc');
-
-            function scrollPage(data) {
-                $('html, body').stop().animate({scrollTop:data.el.offset().top + (data.scroll && data.scroll.length > 1 ? pixelToVw(data.scroll[0], data.scroll[1]) : 1)}, 500);
-            }
-
-            $window.on('load', function() {
-                try {
-               scrollPage(paramsData[anc]);
-            } catch(err) {
-               // console.log('해당하는 앵커 영역이 없습니다.');
-            }
-            });
-        },
-        bannerSwiper: function () {
-            var reSwiper = new Swiper('.swiper-container.banner-swiper', {
+        swiperEvt() {
+            // 메인페이지 후기 스와이퍼
+            var reviewSwiper = new Swiper('.swiper-container.review_swiper', {
                 slidesPerView: 'auto',
-                centeredSlides: true, 
-                pagination: { 
-                    el: ".swiper-pagination",
-                    type: 'bullets',
-                    clickable: true, 
-                },
-                loop: false,
+                loop: true,
                 // loopedSlides: 4,
                 speed: 700,
-                autoplay: {
-                    delay: 3000,
-                    disableOnInteraction: false
-                },
-                navigation:{
-                    nextEl:".swiper-button-next",
-                    prevEl:".swiper-button-prev", 
-                },
+                autoplay: true,
                 breakpoints: {
                     769: {
                         
@@ -99,25 +19,63 @@
                 },
             });
         },
+       
+        headerNameEvt() {
+            var pageUrl = window.location.href; 
+            $WINDOW.on('load', function(){ 
+                setTimeout(() => {
+                    if (pageUrl) {
+                        var $isHeaderName = $('.breadcrumb_list_header').text();
+                        var $fixedHeaderName = $('.sec_header .inner_1440 .title');
+            
+                        $fixedHeaderName.text($isHeaderName);
+                    } 
+                }, 100);
+            });
 
-        tabEvt: function() {
-            $('.tab_title li').on('click' , function() {
-                var idx = $(this).index();
-                $(".tab_title li").removeClass("on");
-                $(".tab_title li").eq(idx).addClass("on");
-                $(".tab_cont > div").hide();
-                $(".tab_cont > div").eq(idx).show();
+
+            $WINDOW.load(function() {
+            //     var headerTxt = () => {
+            //         var $isHeaderName = $('.breadcrumb_list_header').text();
+            //         var $fixedHeaderName = $('.sec_header .inner_1440 .title');
+        
+            //         $fixedHeaderName.text($isHeaderName);
+            //     }
+            //     headerTxt();
+            // alert()
             })
         },
 
+        tabEvt() {
+            // 법무법인 탭 기능
+            var lawyerTab = () => {
+                let $tabList = $('.sec_law-firm .contents_area .tab_list ul li');
+                let $contentsList = $('.sec_law-firm .tab_contents .contents_list');
+                
+                $tabList.on('click', function() {
+                    let $this = $(this);
+                    let $idx = $this.index();
+                    let $isName = $('.breadcrumb_list_name');
+                    let $fixedName = $contentsList.eq($idx).find('.lawyer_name').text();
 
+                    $tabList.eq($idx).addClass('on').siblings().removeClass('on');
+                    $contentsList.eq($idx).addClass('on').siblings().removeClass('on');
+
+
+                    $isName.text($fixedName);
+                    
+
+                })
+            }
+            lawyerTab();
+        },
+    
         init: function() {
-            this.anchorEvt();
-            this.bannerSwiper();
+            this.swiperEvt();
+            this.headerNameEvt();
             this.tabEvt();
         }
     };
-
-    mainIndex.init();
-
-})();
+    
+    MAININDEX.init();
+})
